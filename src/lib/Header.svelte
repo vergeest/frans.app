b971cd49f6314341dadf321281fae40346a3feba
Frans Vergeest <frans@vergeest.dev>
2024-07-14 23:34:19 +0200

    removed hamburger menu icon

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,10 +6,10 @@
 		<span class="text-orange/50">/</span>
 		<a class="inline-block text-orange transition-colors md:hover:text-green" href="/">home</a>
 	</div>
-	<div class="p-6 transition-colors md:p-8 md:hover:text-green">
+	<!-- <div class="p-6 transition-colors md:p-8 md:hover:text-green">
 		<svg width="36" height="18" viewBox="0 0 36 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M2 2H34" stroke="currentColor" stroke-width="2.5" />
 			<path d="M2 10H34" stroke="currentColor" stroke-width="2.5" />
 		</svg>
-	</div>
+	</div> -->
 </div>
