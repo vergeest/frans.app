1e7bfe5050bee1742f6b5ced7416255ad178c374
Frans Vergeest <frans@vergeest.dev>
2024-08-10 23:21:42 +0200

    Removed View Count Component

--- src/lib/HomeGrid.svelte
+++ src/lib/HomeGrid.svelte
@@ -1,12 +1,9 @@
 <script>
 	import BlogPostGridLink from './BlogPostGridLink.svelte';
-	import ViewCountGrid from './ViewCountGrid.svelte';
 </script>
 
 <section class="mx-auto w-[1080px] max-w-full px-6">
-	<div class="grid-flow-rows grid items-stretch gap-8 lg:grid-cols-3">
-		<ViewCountGrid />
-
+	<div class="grid-flow-rows grid items-stretch gap-8 lg:grid-cols-2">
 		<div
 			class="flex items-center justify-center rounded-2xl border border-white/20 bg-black p-8 text-xl text-white/80"
 		>
